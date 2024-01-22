f790a15a40607cbac47ab9f92d46df3b8c7c30e7
Joshua Taylor <89949243+JTaylor9898@users.noreply.github.com>
2024-01-21 16:40:17 -0800

    First Attempt to upload Assignment

---
+++ CSC450_CT1_mod1-2/main.cpp
@@ -0,0 +1,24 @@
+// Include Library Statements
+#include<iostream>
+#include<conio.h>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{
+    double myMoney = 1000.50; //this should be printed out
+
+    // Standard Output Statement
+    cout<<"Please be sure to correct all syntax errors in this program" <<endl;
+    cout<<"I have corrected all errors for this program." <<endl;
+    cout<<"The total amount of money available is = "<< myMoney <<endl;
+
+    // Wait For Output Screen
+    getch();
+    cout<< "You completed the Assignment!" <<endl;
+
+    // Main Function return Statement
+    return 0;
+}
